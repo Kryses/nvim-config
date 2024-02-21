524bd57ed34fdc45cb0f5322033ef344379b976c
Chad Provencher <thechad802@gmail.com>
2024-02-21 11:38:07 -0800

    added lua cmp

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,6 +3,7 @@ return {
   "AstroNvim/astrocommunity",
   {import = "astrocommunity.git.octo-nvim"},
   {import = "astrocommunity.completion.copilot-lua"},
+  {import = "astrocommunity.completion.copilot-lua-cmp"},
 
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
