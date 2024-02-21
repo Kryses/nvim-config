12ebed475c800234c22b4ce794880d58fe359162
Chad Provencher <thechad802@gmail.com>
2024-02-21 14:51:14 -0800

    adds python pack

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,10 +4,9 @@ return {
   {import = "astrocommunity.git.octo-nvim"},
   {import = "astrocommunity.completion.copilot-lua"},
   {import = "astrocommunity.completion.copilot-lua-cmp"},
+  {import = "astrocommunity.completion.copilot-lua-cmp"},
+  {import = "astrocommunity.pack.python"},
 
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
