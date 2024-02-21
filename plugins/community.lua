916f27be0a69277509e63570e39a6077e6fca7b2
Chad Provencher <thechad802@gmail.com>
2024-02-21 11:29:45 -0800

    fixes lua

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,7 +2,7 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   {import = "astrocommunity.git.octo-nvim"},
-  {import = "astrocommunity.completion.copilot-lu"},
+  {import = "astrocommunity.completion.copilot-lua"},
 
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
