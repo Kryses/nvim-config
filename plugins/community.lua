0b76980aad75e4d67f218ce7ac9ec4056992e583
Chad Provencher <thechad802@gmail.com>
2024-02-23 15:54:00 -0800

    added diffview and neogit

--- plugins/community.lua
+++ plugins/community.lua
@@ -6,6 +6,8 @@ return {
   {import = "astrocommunity.completion.copilot-lua-cmp"},
   {import = "astrocommunity.completion.copilot-lua-cmp"},
   {import = "astrocommunity.pack.python"},
+  {import = "astrocommunity.git.diffview-nvim"},
+  {import = "astrocommunity.git.neogit"},
 
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
